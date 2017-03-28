a1dcd49199a6cebbaaaefde86c28b892be982ae7
Your Name <d.holleman700@gmail.com>
2017-03-28 22:30:58 +0200

    [Unstable] Dashboard WIP /w Bootstrap

--- src/Dh/MainBundle/Controller/DefaultController.php
+++ src/Dh/MainBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('DhMainBundle:Default:index.html.twig');
+        return $this->render('DhMainBundle:Dash:index.html.twig');
     }
 }
