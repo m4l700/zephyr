ff8c4c88cd321b20e1964cdccddfe4663c5f288e
Your Name <d.holleman700@gmail.com>
2017-03-30 18:43:19 +0200

    [Unstable] Frontend fixes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Dh\MainBundle\DhMainBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
