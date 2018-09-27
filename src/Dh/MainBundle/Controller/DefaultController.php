a344c944127faf4b39600077c03c28092097ec60
m4l700 <d.holleman700@gmail.com>
2018-09-27 21:44:47 +0200

    [Unstable] CRUD/frontend fixes

--- src/Dh/MainBundle/Controller/DefaultController.php
+++ src/Dh/MainBundle/Controller/DefaultController.php
@@ -25,13 +25,22 @@ class DefaultController extends Controller
 
       //RSS Entity
       $RSS = $this->getDoctrine()->getRepository('DhMainBundle:Rss');
-      $RSSfeed = $RSS->findAll();
+      $RSSfeed = $RSS->findAll(); //Find all feeds
+      $RSSactiveDashboard = $RSS->findBy(array('activeDashboard' => '1')); //get feeds that are active on dashboard
+
+      //Count active RSS feeds on dashboard
+      $rssCountActive = $rssFeed->countRssFeeds($RSSactiveDashboard);
+
+      //Count all feeds
+      $rssCount = $rssFeed->countRssFeeds($RSSfeed);
 
       //Renders template
       return $this->render('DhMainBundle:Dash:index.html.twig',array(
         'username' => $username,
         'feed' => $rssFeed,
         'rssEnt' => $RSSfeed,
+        'rssCountActive' => $rssCountActive,
+        'rssCount' => $rssCount,
       ));
     }
 
